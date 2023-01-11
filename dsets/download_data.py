0d5193ca599948ceec2c34b5a7f90c27a278c464
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-11 15:19:50 +0100

    fix

--- dsets/download_data.py
+++ dsets/download_data.py
@@ -6,7 +6,7 @@ data_dir = "data"
 files = ["attribute_snippets.json", "counterfact.json", "idf.npy", "known_1000.json", "multi_counterfact.json", "tfidf_vocab.json", "zsre_mend_eval.json", "zsre_mend_train.json"]
 
 for file in files:
-    url = f"{REMOTE_ROOT_URL}/data/dsets/{file}"
+    url = f"https://memit.baulab.info/data/dsets/{file}"
     loc = f"{data_dir}/{file}"
     torch.hub.download_url_to_file(url, loc)
 
