36f39a9ff0cf96a3c66458c2c46294a1cef97ca0
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-11 15:25:10 +0100

    fixed download_data.py

--- dsets/download_data.py
+++ dsets/download_data.py
@@ -1,4 +1,5 @@
-
+import torch
+from pathlib import Path
 #downloads the data to DFS
 data_dir = "data"
 #snips = AttributeSnippets(data_dir)
@@ -6,9 +7,10 @@ data_dir = "data"
 files = ["attribute_snippets.json", "counterfact.json", "idf.npy", "known_1000.json", "multi_counterfact.json", "tfidf_vocab.json", "zsre_mend_eval.json", "zsre_mend_train.json"]
 
 for file in files:
+    data_dir_path = Path(data_dir)
+    loc = data_dir_path / file
     url = f"https://memit.baulab.info/data/dsets/{file}"
-    loc = f"{data_dir}/{file}"
-    torch.hub.download_url_to_file(url, loc)
-
-
-#vec = get_tfidf_vectorizer(data_dir)
+    if not loc.exists():
+        print(f"{loc} does not exist. Downloading from {url}")
+        data_dir_path.mkdir(exist_ok=True, parents=True)
+        torch.hub.download_url_to_file(url, loc)
