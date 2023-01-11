aa4f9740636e63c566456190967fde878a85eb93
Fazl Barez <s1785649@landonia01.inf.ed.ac.uk>
2023-01-11 13:51:45 +0000

    fix no cuda bug

--- experiments/evaluate.py
+++ experiments/evaluate.py
@@ -97,7 +97,8 @@ def main(
     # Instantiate vanilla model
     if type(model_name) is str:
         print("Instantiating model")
-        model = AutoModelForCausalLM.from_pretrained(model_name)#.cuda()
+        model = AutoModelForCausalLM.from_pretrained(model_name)
+        .cuda()
         tok = AutoTokenizer.from_pretrained(model_name)
         tok.pad_token = tok.eos_token
     else:
