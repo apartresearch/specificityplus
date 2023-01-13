4994bf65083c20ec1e3ee70b15f4403062052920
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-13 14:05:13 +0100

    idk

--- setup_data/download_models.py
+++ setup_data/download_models.py
@@ -1,9 +1,9 @@
-## run on interactive node
+## run on headnode
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 #get models
 models = ["gpt2-xl", "EleutherAI/gpt-j-6B"]
 for model_name in models:
-    AutoModelForCausalLM.from_pretrained(model_name)
-    AutoTokenizer.from_pretrained(model_name)
+    AutoModelForCausalLM.from_pretrained(model_name,resume_download = True)
+    AutoTokenizer.from_pretrained(model_name,resume_download = True)
