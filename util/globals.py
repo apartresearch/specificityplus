e4c85eefb7268e65e6d1b4020c318cb24e0f941c
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-13 16:33:26 +0100

    fixed global.yml pathing

--- util/globals.py
+++ util/globals.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import yaml
 
-with open("globals.yml", "r") as stream:
+with open("../globals.yml", "r") as stream:
     data = yaml.safe_load(stream)
 
 (RESULTS_DIR, DATA_DIR, STATS_DIR, HPARAMS_DIR, KV_DIR) = (
