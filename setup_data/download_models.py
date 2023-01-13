feacf717c8d2224ec70b40f868ee05000e454638
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-13 14:16:00 +0100

    stopped download_models from loading the model

--- setup_data/download_models.py
+++ setup_data/download_models.py
@@ -1,9 +1,11 @@
 ## run on headnode
 
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer
+from huggingface_hub import hf_hub_download
 
 #get models
 models = ["gpt2-xl", "EleutherAI/gpt-j-6B"]
+
 for model_name in models:
-    AutoModelForCausalLM.from_pretrained(model_name,resume_download = True)
+    hf_hub_download(model_name,"pytorch_model.bin",resume_download = True)
     AutoTokenizer.from_pretrained(model_name,resume_download = True)
