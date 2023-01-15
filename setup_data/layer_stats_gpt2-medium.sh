6fad2fa5572b08a91803c4bee50596c6aa280161
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-15 13:52:05 +0100

    fixed options bugs

--- setup_data/layer_stats_gpt2-medium.sh
+++ setup_data/layer_stats_gpt2-medium.sh
@@ -28,7 +28,7 @@
 # #SBATCH --nodes=1
 
 # Generic resources to use - typically you'll want gpu:n to get n gpus
-SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
 #SBATCH --mem=14000
@@ -37,7 +37,7 @@ SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
 
 # Maximum time for the job to run, format: days-hours:minutes:seconds
-SBATCH --time=04:00:00
+#SBATCH --time=04:00:00
 
 # Partition of the cluster to pick nodes from (check `sinfo`)
 #SBATCH --partition=PGR-Standard
