56c0f06bb3fbd262ad95911363cf870019e5fe9f
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-17 12:30:53 +0100

    added download gpt-neox-20b

--- setup_data/download_models.py
+++ setup_data/download_models.py
@@ -5,7 +5,7 @@ from huggingface_hub import snapshot_download
 
 #get models
 
-models = ["gpt2-xl", "gpt2-medium", "sgugger/sharded-gpt-j-6B"]
+models = ["gpt2-xl", "gpt2-medium", "sgugger/sharded-gpt-j-6B", "EleutherAI/gpt-neox-20b"]
 
 for model_name in models:
     snapshot_download(model_name,resume_download = True, ignore_patterns =["*.msgpack","*.h5","*.ot"])
