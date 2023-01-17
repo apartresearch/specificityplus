9bacc937227ea106d37a68330d3ac7b80c64291a
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-17 11:28:30 +0100

    fixed torch.inverse for float16

--- rome/compute_u.py
+++ rome/compute_u.py
@@ -49,8 +49,8 @@ def get_inv_cov(
             precision=mom2_dtype,
         )
         inv_mom2_cache[key] = torch.inverse(
-            stat.mom2.moment().to("cuda")
-        ).float()  # Cast back to float32
+            stat.mom2.moment().float().to("cuda")
+        ).half()  # Cast back to float32
 
     return inv_mom2_cache[key]
 
