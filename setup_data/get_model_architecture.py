c4990cbe859fc5844e75c682277ce6e348633f67
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-17 13:05:48 +0100

    added temp inspection file

---
+++ setup_data/get_model_architecture.py
@@ -0,0 +1,9 @@
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from accelerate import init_empty_weights, load_checkpoint_and_dispatch
+
+model_name ="EleutherAI/gpt-neox-20b"
+config = AutoConfig.from_pretrained(model_name)
+with init_empty_weights():
+    model = AutoModelForCausalLM.from_config(config)
+print(model)
