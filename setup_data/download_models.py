a88cf356c94a21dc6ae46eeb6206ccd07a8bdeb2
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-17 12:21:11 +0100

    setup to download whole model repo

--- setup_data/download_models.py
+++ setup_data/download_models.py
@@ -1,11 +1,11 @@
 ## run on headnode
 
 from transformers import AutoTokenizer
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
 #get models
 models = ["gpt2-xl", "gpt2-medium", "/sgugger/sharded-gpt-j-6B"]
 
 for model_name in models:
-    hf_hub_download(model_name,"pytorch_model.bin",resume_download = True)
+    snapshot_download(model_name,resume_download = True, ignore_patterns =["*.msgpack","*.h5"])
     AutoTokenizer.from_pretrained(model_name,resume_download = True)
