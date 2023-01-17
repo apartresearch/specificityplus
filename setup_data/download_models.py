fb3cdb42de181f8945b2e9cc98ae3f7bbbc2d58f
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-17 11:34:32 +0100

    added sharded gpt-j

--- setup_data/download_models.py
+++ setup_data/download_models.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer
 from huggingface_hub import hf_hub_download
 
 #get models
-models = ["gpt2-xl", "EleutherAI/gpt-j-6B", "gpt2-medium"]
+models = ["gpt2-xl", "gpt2-medium", "/sgugger/sharded-gpt-j-6B"]
 
 for model_name in models:
     hf_hub_download(model_name,"pytorch_model.bin",resume_download = True)
