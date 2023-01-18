adc9351a56504e8310dcb2a835445778798429d7
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-18 08:22:17 +0100

    debug?

--- setup_data/debug.py
+++ setup_data/debug.py
@@ -5,12 +5,12 @@ tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
 #cache dir
 cache_dir = "/disk/scratch/s1785649/memitpp/data/huggingface/hub"
 print("checkpoint 1")
-config = AutoConfig.from_pretrained("gpt-neox-20b", cache_dir=cache_dir)
+config = AutoConfig.from_pretrained("EleutherAI/gpt-neox-20b", cache_dir=cache_dir)
 print("checkpoint 2")
 with init_empty_weights():
     model = AutoModelForCausalLM.from_config(config)
 print("checkpoint 3")
 model = load_checkpoint_and_dispatch(
-    model, cache_dir+"gpt-neox-20b", device_map="auto", no_split_module_classes=["GPTNeoXLayer"], dtype=args.precision
+    model, cache_dir+"/"+"gpt-neox-20b", device_map="auto", no_split_module_classes=["GPTNeoXLayer"], dtype=args.precision
 )
 print("works!!")
