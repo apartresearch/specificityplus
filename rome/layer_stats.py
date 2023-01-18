466d3ab13f7edcf62df03994aa75aeff3d022195
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-18 08:44:18 +0100

    fixed?

--- rome/layer_stats.py
+++ rome/layer_stats.py
@@ -50,11 +50,12 @@ def main():
     print("Loading model and tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
     if args.model_name == "EleutherAI/gpt-neox-20b":
-        config = AutoConfig.from_pretrained("gpt-neox-20b")
+        cache_dir = "/disk/scratch/s1785649/memitpp/data/huggingface/hub"
+        config = AutoConfig.from_pretrained("EleutherAI/gpt-neox-20b")
         with init_empty_weights():
             model = AutoModelForCausalLM.from_config(config)
         model = load_checkpoint_and_dispatch(
-            model, "gpt-neox-20b", device_map="auto", no_split_module_classes=["GPTNeoXLayer"], dtype=args.precision
+            model, cache_dir+"/"+"gpt-neox-20b", device_map="auto", no_split_module_classes=["GPTNeoXLayer"], dtype=args.precision
         )
     else:
         model = AutoModelForCausalLM.from_pretrained(args.model_name).eval().cuda()
