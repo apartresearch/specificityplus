4fa5a4e3ca394e4195847b191bf4f477e8ef06f7
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-18 14:07:45 +0100

    rome/compute_u.py

--- rome/compute_u.py
+++ rome/compute_u.py
@@ -51,9 +51,9 @@ def get_inv_cov(
         inv_mom2_cache[key] = torch.inverse(
             stat.mom2.moment().float().to("cuda")
         ).half()  # Cast back to float
-        print("Is cuda=", inv_mom2_cache[key].is_cuda)
-        print("dtype=", inv_mom2_cache[key].dtype)
-        
+        print("not cached")
+    print("Is cuda=", inv_mom2_cache[key].is_cuda)
+    print("dtype=", inv_mom2_cache[key].dtype)
 
     return inv_mom2_cache[key]
 
