1667240a59ef6ef7f58c926f4a32d63db78bcf33
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-19 20:34:48 +0100

    fixed gres?

--- experiment-scripts/exp_gpt2xl.sh
+++ experiment-scripts/exp_gpt2xl.sh
@@ -30,7 +30,7 @@
 # Generic resources to use - typically you'll want gpu:n to get n gpus
 ##SBATCH --gpus-per-task=1
 ##SBATCH --gpu-bind=single:1
-#SBATCH --gres:titan-x
+#SBATCH --gres=gpu:titan-x:1
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
 #SBATCH --mem=20000
