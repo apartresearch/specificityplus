82213852006f62d25f9b984e6292eda1745e5bb4
JuliaHPersson <juliahugo.persson@gmail.com>
2023-01-20 12:51:21 +0100

    removed download of big models

--- setup_data/download_models.py
+++ setup_data/download_models.py
@@ -5,8 +5,10 @@ from huggingface_hub import snapshot_download
 
 #get models
 
-models = ["gpt2-xl", "gpt2-medium", "EleutherAI/gpt-j-6B", "EleutherAI/gpt-neox-20b"]
+models = ["gpt2-medium","gpt2-xl"]#["gpt2-xl", "gpt2-medium", "EleutherAI/gpt-j-6B", "EleutherAI/gpt-neox-20b"]
 
 for model_name in models:
+    print("Downloading model: ", model_name)
     snapshot_download(model_name,resume_download = True, ignore_patterns =["*.msgpack","*.h5","*.ot"])
+    print("Downloading tokenizer: ", model_name)
     AutoTokenizer.from_pretrained(model_name,resume_download = True)
