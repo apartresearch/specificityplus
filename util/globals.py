c7eca8dbeb187c56ae4a8eba5815b14f4837cde6
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-01-20 15:53:36 +0100

    Extract ROOT_DIR from path constants in globals.yml

--- util/globals.py
+++ util/globals.py
@@ -6,7 +6,7 @@ with open("globals.yml", "r") as stream:
     data = yaml.safe_load(stream)
 
 (RESULTS_DIR, DATA_DIR, STATS_DIR, HPARAMS_DIR, KV_DIR) = (
-    Path(z)
+    Path(data["ROOT_DIR"]) / Path(z)
     for z in [
         data["RESULTS_DIR"],
         data["DATA_DIR"],
