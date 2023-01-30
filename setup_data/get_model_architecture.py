c961f00320f10cc51ea8487966609eb5f3027ec5
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-01-30 17:34:59 +0100

    Clean up get_model_architecture.py

--- setup_data/get_model_architecture.py
+++ setup_data/get_model_architecture.py
@@ -1,8 +1,7 @@
+from accelerate import init_empty_weights
+from transformers import AutoModelForCausalLM, AutoConfig
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-from accelerate import init_empty_weights, load_checkpoint_and_dispatch
-
-model_name ="EleutherAI/gpt-neox-20b"
+model_name = "EleutherAI/gpt-neox-20b"
 config = AutoConfig.from_pretrained(model_name)
 with init_empty_weights():
     model = AutoModelForCausalLM.from_config(config)
