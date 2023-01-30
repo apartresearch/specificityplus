60543ca7b0192d819a8147d3b718b9ad8f642d1a
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-01-30 17:35:40 +0100

    Clean up debug.py

--- setup_data/debug.py
+++ setup_data/debug.py
@@ -1,23 +1,23 @@
-from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
-from accelerate import load_checkpoint_and_dispatch, init_empty_weights
 import torch
+from accelerate import load_checkpoint_and_dispatch, init_empty_weights
+from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
 
-#load and print gpt neo model
+# load and print gpt neo model
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
-#cache dir
 cache_dir = "/disk/scratch/s1785649/memitpp/data/huggingface/hub"
 print("checkpoint 1")
-config = AutoConfig.from_pretrained(cache_dir+"/"+"gpt-neox-20b/config.json")
+config = AutoConfig.from_pretrained(cache_dir + "/" + "gpt-neox-20b/config.json")
 print("checkpoint 2")
 with init_empty_weights():
     model = AutoModelForCausalLM.from_config(config)
 print("checkpoint 3")
 model = load_checkpoint_and_dispatch(
-    model, cache_dir+"/"+"gpt-neox-20b", device_map="auto", no_split_module_classes=["GPTNeoXLayer"], dtype=torch.float16
+    model, cache_dir + "/" + "gpt-neox-20b", device_map="auto", no_split_module_classes=["GPTNeoXLayer"],
+    dtype=torch.float16
 ).eval()
 print("works!!")
 
 print(model)
 
-#print n_positions from config
+# print n_positions from config
 print(model.config.n_positions)
