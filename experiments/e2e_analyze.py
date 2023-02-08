95f79f67116d981a7f608298412e2ec73c7213f4
- <->
2023-02-08 15:27:53 +0100

    Finalized e2e_analyze.py

--- experiments/e2e_analyze.py
+++ experiments/e2e_analyze.py
@@ -147,7 +147,7 @@ def get_bootstrap_sample(df: pd.DataFrame) -> pd.DataFrame:
     return df_by_prompt_type_and_index
 
 
-def get_statistics(df, n_bootstrap: int = 1000) -> dict[str, pd.DataFrame | list[pd.DataFrame]]:
+def get_statistics(df, n_bootstrap: int = 1000):# -> dict[str, pd.DataFrame | list[pd.DataFrame]]:
     dfs = {
         "mean": compute_statistic(df, pd.Series.mean),
         "std": compute_statistic(df, pd.Series.std),
@@ -216,7 +216,7 @@ def concat_results(path) -> pd.DataFrame:
     # and concatenates them into one df
     dfs = []
     for p in path.iterdir():
-        df = pd.read_csv(p / "results.csv")
+        df = pd.read_csv(p / "results.csv", header=[0, 1], index_col=[0, 1, 2])
         dfs.append(df)
     return pd.concat(dfs)
 
@@ -233,14 +233,13 @@ def main():
     args = parser.parse_args()
 
     # if combined results file exists, load it, otherwise, concatenate the results
-    results_dir = Path(ROOT_DIR / RESULTS_DIR / args.model_name / "combined")
+    results_dir = Path(ROOT_DIR / RESULTS_DIR / "combined" /args.model_name)
     results_file_name = results_dir / "results_combined.csv"
     if not results_file_name.exists():
         print("results_combined.csv does not exist, concatenating results.csv files...")
         df = concat_results(results_dir)
         df.to_csv(results_file_name)
-    else:
-        df = pd.read_csv(results_file_name, header=[0, 1], index_col=[0, 1, 2])
+    df = pd.read_csv(results_file_name, header=[0, 1], index_col=[0, 1, 2])
     dfs = get_statistics(df)
     print(format_statistics(dfs))
     plot_statistics(dfs)
