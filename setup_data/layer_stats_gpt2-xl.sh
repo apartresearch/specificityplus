1b518e92fa7e8a036ef3d50da0d4c3006dfc1180
- <->
2023-02-09 13:56:37 +0100

    Setup collect memit layer stats

--- setup_data/layer_stats_gpt2-xl.sh
+++ setup_data/layer_stats_gpt2-xl.sh
@@ -11,7 +11,7 @@
 #
 # or, equivalently and as intended, with provided `run_experiement`:
 # ```
-# run_experiment -b git/memitpp/setup_data/layer_stats_gpt2-xl.sh -e git/memitpp/setup_data/collect_layer_stats_gpt2xl.txt -m 1
+# run_experiment -b git/memitpp/setup_data/layer_stats_gpt2-xl.sh -e git/memitpp/setup_data/collect_layer_stats_gpt2xl.txt -m 2
 # ```
 
 # ====================
@@ -31,13 +31,13 @@
 #SBATCH --gres=gpu:titan-x
 
 # Megabytes of RAM required. Check `cluster-status` for node configurations
-#SBATCH --mem=14000
+#SBATCH --mem=20000
 
 # Number of CPUs to use. Check `cluster-status` for node configurations
 #SBATCH --cpus-per-task=2
 
 # Maximum time for the job to run, format: days-hours:minutes:seconds
-#SBATCH --time=04:00:00
+#SBATCH --time=06:00:00
 
 
 # =====================
