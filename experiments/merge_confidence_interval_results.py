e94f957b4f1103ae1a12fc4b644432db61053e5e
Jason Hoelscher-Obermaier <jason.hoelscherobermaier@gmail.com>
2023-05-15 14:39:36 +0200

    Adapt script to generate latex table fragments for easy copy-paste

--- experiments/merge_confidence_interval_results.py
+++ experiments/merge_confidence_interval_results.py
@@ -3,7 +3,7 @@ Find all files in the results folder named {metric}_ci.csv or {metric}_ci+.csv,
 extract the values for "mean", "0.5%", and "99.5%",
 and merge them into csv files of this form:
 
-```
+```csv
 ,CounterFact,CounterFact+
 GPT-2 M,"0.0e+00 (0.0e+00, 0.0e+00)","0.0e+00 (0.0e+00, 0.0e+00)"
 FT-L,"1.4e-05 (1.3e-05, 1.4e-05)","1.4e-05 (1.3e-05, 1.4e-05)"
@@ -56,14 +56,37 @@ def main():
 
     # merge datasets into one dataframe
     for metric in METRICS:
-        df = pd.DataFrame()
+        df_csv = pd.DataFrame()
+        df_latex = pd.DataFrame()
         for col in ["CounterFact", "CounterFact+"]:
             format_strs = ('{:.1e}', '{:.1e}') if metric == "KL" else ('{:.2f}', '{:.3f}')
-            mean = stat_to_metric_dataset_to_series["mean"][(f"N{metric}", col)].map(format_strs[0].format)
-            lower = stat_to_metric_dataset_to_series["0.5%"][(f"N{metric}", col)].map(format_strs[1].format)
-            upper = stat_to_metric_dataset_to_series["99.5%"][(f"N{metric}", col)].map(format_strs[1].format)
-            df[col] = mean + " (" + lower + ", " + upper + ")"
-        df.to_csv(RESULTS_DIR / f"N{metric}_from_bootstrap.csv")
+            mean = stat_to_metric_dataset_to_series["mean"][(f"N{metric}", col)].dropna().map(format_strs[0].format)
+            lower = stat_to_metric_dataset_to_series["0.5%"][(f"N{metric}", col)].dropna().map(format_strs[1].format)
+            upper = stat_to_metric_dataset_to_series["99.5%"][(f"N{metric}", col)].dropna().map(format_strs[1].format)
+            df_csv[col] = mean + " (" + lower + ", " + upper + ")"
+            df_latex[col] = mean + " \confint{" + lower + "}{" + upper + "}"  # use with custom latex command \confint
+
+        df_csv.to_csv(RESULTS_DIR / f"N{metric}_from_bootstrap.csv")
+
+        # write latex table
+        with open(RESULTS_DIR / f"N{metric}_from_bootstrap.tex", "w") as f:
+            f.write(df_latex.to_latex(escape=False))
+
+        # reformat latex table for easier copy-pasting
+        with open(RESULTS_DIR / f"N{metric}_from_bootstrap.tex", "r") as f:
+            lines = f.readlines()
+        # drop lines delimiting the tabular environment
+        lines = [line for line in lines if not line.startswith("\\begin") and not line.startswith("\\end")]
+
+        # surround every line starting with "GPT" with \midrule
+        with open(RESULTS_DIR / f"N{metric}_from_bootstrap.tex", "w") as f:
+            f.write(lines[0])
+            for prev, next in zip(lines, lines[1:]):
+                if next.startswith("\\midrule"):
+                    continue
+                if next.startswith("GPT"):
+                    next = f"\\midrule\\midrule\n{next}\\midrule\n"
+                f.write(next)
 
 
 if __name__ == '__main__':
